aee49e58a13027d253411834c8b5880650141e28
tzvieli <109800331+tzvieli@users.noreply.github.com>
2022-12-16 13:08:36 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,26 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.type === 'text-highlighted') {
-    let selectedText = message.selectedText;
-    // Code to handle the selected text goes here
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'convertText') {
+    document.getSelection().getRangeAt(0).deleteContents();
+    document.getSelection().getRangeAt(0).insertNode(document.createTextNode(request.convertedText));
   }
 });
+
+document.addEventListener('mouseup', function(event) {
+  let selectedText = document.getSelection().toString().trim();
+  if (selectedText !== '') {
+    let language;
+    if (/^[\u0590-\u05FF]+$/.test(selectedText)) {
+      language = 'hebrew';
+    } else {
+      language = 'english';
+    }
+    chrome.runtime.sendMessage({
+      action: 'convertText',
+      text: selectedText,
+      language: language
+    }, function(response) {
+      console.log(response.convertedText);
+    });
+  }
+});
+
